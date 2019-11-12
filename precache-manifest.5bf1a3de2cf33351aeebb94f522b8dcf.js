955bdbd696fb0949f2b12d02430226426919c11d
oikuman <oikuma@gmail.com>
2019-11-12 19:20:49 +0200

    Updates

---
+++ precache-manifest.5bf1a3de2cf33351aeebb94f522b8dcf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d1949dd47c4a37a4a781118c1d0d65",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1ff05e2fd3bfc3a96258",
+    "url": "/goit-react-hw-01-components/static/css/2.921ecf0d.chunk.css"
+  },
+  {
+    "revision": "cf379ae7d530ae341a12",
+    "url": "/goit-react-hw-01-components/static/css/main.e1c751ec.chunk.css"
+  },
+  {
+    "revision": "1ff05e2fd3bfc3a96258",
+    "url": "/goit-react-hw-01-components/static/js/2.4cf4022f.chunk.js"
+  },
+  {
+    "revision": "cf379ae7d530ae341a12",
+    "url": "/goit-react-hw-01-components/static/js/main.ac6f82d4.chunk.js"
+  },
+  {
+    "revision": "620ceb3fc25e2e9c8f81",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.a6ab4004.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/goit-react-hw-01-components/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/goit-react-hw-01-components/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/goit-react-hw-01-components/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/goit-react-hw-01-components/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/goit-react-hw-01-components/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "7c3daf12b706645b5d3710f863a4da04",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-300.7c3daf12.woff2"
+  },
+  {
+    "revision": "8dc95fab9cf98d02ca8d76e97d3dff60",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-300.8dc95fab.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/goit-react-hw-01-components/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "3654d75cd51a194489728b94ddb7d1f4",
+    "url": "/goit-react-hw-01-components/static/media/oswald-v26-latin-regular.3654d75c.woff"
+  },
+  {
+    "revision": "d54dcd9a6bb8a523ffa27a4a37012710",
+    "url": "/goit-react-hw-01-components/static/media/oswald-v26-latin-regular.d54dcd9a.woff2"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "/goit-react-hw-01-components/static/media/raleway-v14-latin-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "60b344eb8dd676754364fc5ae4500d62",
+    "url": "/goit-react-hw-01-components/static/media/raleway-v14-latin-regular.60b344eb.woff"
+  }
+]);
